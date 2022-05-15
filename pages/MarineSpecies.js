38d4837e177b460edf60be55bc37cd731126e01f
JonathanGzzBen <jonathangzzben@gmail.com>
2022-05-15 14:04:14 -0500

    Change layout of marine species

--- pages/MarineSpecies.js
+++ pages/MarineSpecies.js
@@ -6,25 +6,10 @@ import { getAllMarineSpecies } from "../services/marinespecies";
 
 export default function MarineEspecies() {
   const [marineSpecies, setMarineSpecies] = useState([]);
-  const [marineSpeciesRows, setMarineSpeciesRows] = useState([]);
   useEffect(() => {
-    let rows = [];
     getAllMarineSpecies().then((marineSpeciesRes) => {
       setMarineSpecies(marineSpeciesRes);
-      console.log(marineSpeciesRes);
-      let row = [];
-      marineSpeciesRes.forEach((s) => {
-        row.push(s);
-        if (row.length >= 3) {
-          rows.push(row);
-          row = [];
-        }
-      });
     });
-    setMarineSpeciesRows(rows);
-    console.log(marineSpecies);
-    console.log("Rows", rows);
-    console.log("Marine Species Rows", marineSpeciesRows);
   }, []);
   return (
     <>
@@ -43,34 +28,20 @@ export default function MarineEspecies() {
             These species of fish are endangered
           </div>
         </div>
-        {marineSpeciesRows.map((marineSpeciesRow, index) => (
-          <div className="columns" key={index}>
-            {marineSpeciesRow.map((marineSpecies, index) => (
-              <div className="column" key={index}>
-                <div key={marineSpecies.scientificName} className="card">
-                  <div className="card-image">
-                    <figure className="image is-4by3">
-                      <img
-                        src={marineSpecies.imageUrl}
-                        alt="Image image"
-                        width="100px"
-                        height="100px"
-                      />
-                    </figure>
-                  </div>
-                  <p className="title">{marineSpecies.aliases}</p>
-                  <div className="card-content">
-                    <div
-                      className="content"
-                      dangerouslySetInnerHTML={{
-                        __html: marineSpecies.biology,
-                      }}
-                    ></div>
-                    {/* <div className="content">{marineSpecies.biology}</div> */}
-                  </div>
-                </div>
-              </div>
-            ))}
+        {/* Middle */}
+        {marineSpecies.map((marineSpecies, index) => (
+          <div key={index} className="columns">
+            <div className="column">
+              <img src={marineSpecies.imageUrl} />
+            </div>
+            <div className="column is-two-thirds">
+              <h2 className="title is-2">{marineSpecies.aliases}</h2>
+              <h3 className="subtitle is-3">{marineSpecies.scientificName}</h3>
+              <p
+                className="content"
+                dangerouslySetInnerHTML={{ __html: marineSpecies.biology }}
+              ></p>
+            </div>
           </div>
         ))}
       </Layout>
