3ee9c519997b04dcb603690d428992ad5251cd15
JonathanGzzBen <jonathangzzben@gmail.com>
2022-05-15 11:23:24 -0500

    Add basic Bulma layout

---
+++ components/NavBar.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import Image from "next/image";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar" role="navigation" aria-label="main navigation">
+      <div className="navbar-brand">
+        <a className="navbar-item" href="https://bulma.io">
+          <Image
+            src="https://bulma.io/images/bulma-logo.png"
+            width="112"
+            height="28"
+            alt="logo"
+          />
+        </a>
+
+        <a
+          role="button"
+          className="navbar-burger"
+          aria-label="menu"
+          aria-expanded="false"
+          data-target="navbarBasicExample"
+        >
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+        </a>
+      </div>
+
+      <div id="navbarBasicExample" className="navbar-menu">
+        <div className="navbar-start">
+          <a className="navbar-item">Home</a>
+          <a className="navbar-item">Documentation</a>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
